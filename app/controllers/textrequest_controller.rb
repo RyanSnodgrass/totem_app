971d82e8b9119be4722798f38bdb1841f8efb821
Riaz Virani <riaz.n.virani@gmail.com>
2014-08-23 15:46:48 -0400

    removed csrf stuff

--- app/controllers/textrequest_controller.rb
+++ app/controllers/textrequest_controller.rb
@@ -1,12 +1,22 @@
 class TextrequestController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+  before_action :twilio_client
+
   def create
-    @message = @client.account.messages.create({:to => "+16786369295",
+    from_number = request[:From]
+    puts "From number is #{from_number}"
+    @message = @client.account.messages.create({:to => from_number,
                                                 :from => "+14043416358",
                                                 :body => "Yes it works!"})
-    puts @message.text.render_to_string
 
     render plain: "OK"
+  end
+
+  private
+
+  def twilio_client
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
   end
 
 end
