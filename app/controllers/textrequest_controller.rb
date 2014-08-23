c5b315ded521d8d3fa03922255ddd0a56229d195
Riaz Virani <riaz.n.virani@gmail.com>
2014-08-23 15:15:41 -0400

    added plain response to create action

--- app/controllers/textrequest_controller.rb
+++ app/controllers/textrequest_controller.rb
@@ -4,8 +4,9 @@ class TextrequestController < ApplicationController
     @message = @client.account.messages.create({:to => "+16786369295",
                                                 :from => "+14043416358",
                                                 :body => "Yes it works!"})
-
     puts @message.text.render_to_string
+
+    render plain: "OK"
   end
 
 end
